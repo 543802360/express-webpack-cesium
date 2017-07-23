b2540cef7f48e44a3a5f2795ca136b4e37dcaaf3
布鲁斯 <liushuo_whu@163.com>
2017-07-23 21:56:40 +0800

    add an example

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Webpack练习一' });
+    res.render('index', { title: 'WebpackCesiumDemo' });
 });
 
 
